a93d909a8f51c3bd008514629a950efbb2cde5b1
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2015-08-22 21:42:18 +0200

    Spring Initializr project

---
+++ src/main/java/com/nurkiewicz/progress/SpringStartupProgressDemoApplication.java
@@ -0,0 +1,12 @@
+package com.nurkiewicz.progress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartupProgressDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartupProgressDemoApplication.class, args);
+    }
+}
