8eb1eeeba4e85b277aadcb9834066e4dae88192b
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2015-08-31 23:10:46 +0200

    Showing beautiful progress + more dependencies to show

--- src/main/java/com/nurkiewicz/progress/SlowBeans.java
+++ src/main/java/com/nurkiewicz/progress/SlowBeans.java
@@ -17,7 +17,7 @@ public class SlowBeans implements BeanDefinitionRegistryPostProcessor {
 	@Override
 	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
 		IntStream
-				.range(0, 100)
+				.range(0, 10)
 				.forEach(i -> registry.registerBeanDefinition(
 						"slow-" + i,
 						new RootBeanDefinition(SlowBean.class)));
